a4b52b9a5d428049d1f85d2442ad2c0cfacacfc6
mitul.jariwala <mj695067@gmail.com>
2023-04-02 00:40:58 +0530

    Initial commit with Home page UI

---
+++ src/components/UserCard.js
@@ -0,0 +1,59 @@
+import { View, Text, ImageBackground, StyleSheet, Image } from "react-native";
+
+export default function UserCard({ user }) {
+  return (
+    <ImageBackground source={{ uri: user.coverImage }} style={styles.userCard}>
+      <View style={styles.overlay} />
+
+      {/* Image */}
+      <Image src={user.avatar} style={styles.userImage} />
+      {/* Name */}
+      <View>
+        <Text
+          style={{
+            color: "white",
+            fontSize: 22,
+            fontWeight: "500",
+            marginBottom: 5,
+          }}>
+          {user.name}
+        </Text>
+        <Text style={{ color: "white" }}>@{user.handle}</Text>
+      </View>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 50,
+  },
+  userCard: {
+    backgroundColor: "grey",
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "flex-end",
+    borderRadius: 10,
+    overflow: "hidden",
+    marginBottom: 10,
+  },
+  userImage: {
+    height: 100,
+    width: 100,
+    borderRadius: 100,
+    borderColor: "white",
+    borderWidth: 3,
+    marginRight: 20,
+  },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    // position: "absolute",
+    // top: 0,
+    // bottom: 0,
+    // left: 0,
+    // right: 0,
+    ...StyleSheet.absoluteFillObject,
+  },
+});
